62e48ad4107221e7c54359e7f71e45bb43c7db3c
bittern <bittern@EMAIL.com>
2017-04-14 16:43:33 +0530

    newapp

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :categories
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  
+    root 'blogs#index'
+    resources :blogs do
+      resources :comments
+    end
+ 
+
+   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
